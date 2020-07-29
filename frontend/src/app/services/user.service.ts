8f2dcbd330104c9b6c38014dbd39de79404a527d
ZakaTiwana <61515214+ZakaTiwana@users.noreply.github.com>
2020-07-29 20:28:35 +0500

    frontend: user serivce CRUD funtion created

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url: string;
+  
+  constructor(private http: HttpClient) {
+    this.url = environment.api + "user/";
+  }
+
+  addUser(user_data: FormData) {
+    return this.http.post(this.url, user_data)
+  }
+
+  deleteUser(id: string) {
+    return this.http.delete(this.url.concat(id));
+  }
+  UpdateUser(user: User) {
+    return this.http.put(this.url.concat(user._id), user);
+  }
+  getUser(id: string) {
+    return this.http.get(this.url.concat(id));
+  }
+  getAllUsers() {
+    return this.http.get(this.url);
+  }
+  userLogin(user:User){
+    return this.http.post(this.url.concat("login"),user);
+  }
 }
